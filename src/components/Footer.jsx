27bdec83522e3c195e2b46cc579e256b96ade86a
Çağlar Genç <168332436+rubyyy17@users.noreply.github.com>
2024-09-25 00:48:02 +0700

    deme9

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,14 +10,14 @@ function Footer() {
             
                 <div className='betweenspace' style={{width:"390.53px"}}>
                     <p className='footeryazi1'>Больше фото наших праздников в соцсетях</p>
-                    <a href="https://vk.com/tridevatoetsarstvo" target='_blank'><img src="../src/pictures/Vector.png" alt="" /></a>
+                    <a href="https://vk.com/tridevatoetsarstvo" target='_blank'><img src="../pictures/Vector.png" alt="" /></a>
                 </div>
 
             
             <div>
                 <div className='betweenspace'>
                     <p className='footeryazi1' style={{display:"flex"}}>Хочешь к нам в команду? Отправь свое резюме </p>
-                    <a href="http://t.me/Nastya_expert" target='_blank'><img src="../src/pictures/telegram.png" alt="" /></a>
+                    <a href="http://t.me/Nastya_expert" target='_blank'><img src="../pictures/telegram.png" alt="" /></a>
                 </div>
             </div>
 
@@ -28,14 +28,14 @@ function Footer() {
         <div  className='kutular' >
             <div div className='footertelefonidiv' >
                 <div className='row'>
-                    <img src="../src/pictures/Group.png" alt="" />
+                    <img src="../pictures/Group.png" alt="" />
                     <p className='yazi' style={{fontSize:"16px"}}>+7 (992) 202-08-88</p>
                 </div>
 
             </div>
             <div div className='footertelefonidiv'>
                 <div className='row'>
-                <img src="../src/pictures/harita.png" alt="" />
+                <img src="../pictures/harita.png" alt="" />
                 <p className='adres'>Набережная, 41/1, г. Березники</p>
                 </div>
             </div>
@@ -55,8 +55,8 @@ function Footer() {
 
         <p className='footeryazi1'>Больше фото наших праздников в соцсетях</p>
         <div className='row' style={{margin:"20px 0 20px 0"}}>
-        <img src="../src/pictures/Vector.png" alt="" />
-        <img src="../src/pictures/telegram.png" alt="" />
+        <img src="../pictures/Vector.png" alt="" />
+        <img src="../pictures/telegram.png" alt="" />
         </div>
         <div className='column'>
             <button className='button' >
